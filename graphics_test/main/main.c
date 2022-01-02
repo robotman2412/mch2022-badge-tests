52f3d80a2e7147b64abd682b1bfe2c1a40c16968
RobotMan2412 <julian@scheffers.net>
2022-01-02 20:13:54 +0100

    Messing aroung with the graphics

--- graphics_test/main/main.c
+++ graphics_test/main/main.c
@@ -5,20 +5,29 @@
 #include <sdkconfig.h>
 #include <esp_err.h>
 #include <esp_log.h>
+#include <stdint.h>
+#include <malloc.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <esp_timer.h>
 
 // Screen.
 #include <driver/spi_master.h>
 #include <hal/spi_types.h>
 #include "ili9341.h"
 
+// GFX library.
+#include "pax_gfx.h"
+
 // Configuration.
 #define SPI_MOSI 12
 #define SPI_MISO 13
 #define SPI_SCLK 21
 #define SPI_MAX_TRANSFER 4094
-#define SPI_BUS 2 //VSPI_HOST
+#define SPI_BUS ((spi_host_device_t) 2) //VSPI_HOST
 #define SPI_DMA_CH 2
 
+#define DISPLAY_RST 4
 #define DISPLAY_CS 16
 #define DISPLAY_DCX 17
 
@@ -29,8 +38,6 @@ uint8_t *framebuffer;
 static const char *TAG = "main";
 
 void app_main() {
-    
-	
 	
 	// Let's see.
 	printf("I booted!\n");
@@ -55,7 +62,7 @@ void app_main() {
 	display.spi_bus    = SPI_BUS;
 	display.pin_cs     = DISPLAY_CS;
 	display.pin_dcx    = DISPLAY_DCX;
-	display.pin_reset  = -1;
+	display.pin_reset  = DISPLAY_RST;
 	display.rotation   = 1;
 	display.color_mode = true;
 	display.spi_speed  = 60000000; // 60 MHz
@@ -68,8 +75,24 @@ void app_main() {
 		ESP_LOGI(TAG, "Display initialised.");
 	}
 	
+	// Initialise GFX.
+	framebuffer = (uint8_t *) malloc(ILI9341_BUFFER_SIZE);
+	
 	// Send a test pattern.
-	
-	// There's nothing to do, really.
-	
+	pax_buf_t buf;
+	pax_buf_init(&buf, framebuffer, ILI9341_WIDTH, ILI9341_HEIGHT, PAX_BUF_16_565RGB);
+	while (1) {
+		uint64_t millis = esp_timer_get_time() / 1000;
+		pax_background(&buf, 0x000000);
+		pax_col_t color = pax_col_hsv(millis * 255 / 8000, 255, 255);
+		float a0 = millis / 3000.0 * M_PI;
+		float a1 = fmodf(a0, M_PI * 4) - M_PI * 2;
+		pax_simple_arc(&buf, color, buf.width >> 1, buf.height >> 1, 50, a0, a0 + a1);
+		
+		if (ili9341_write(&display, framebuffer)) {
+			ESP_LOGE(TAG, "Display write failed.");
+			vTaskDelay(3000 / portTICK_PERIOD_MS);
+			esp_restart();
+		}
+	}
 }
