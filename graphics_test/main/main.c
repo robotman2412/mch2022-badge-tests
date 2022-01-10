a50df47c2083d42f32620294632cf61713ff73ff
RobotMan2412 <julian@scheffers.net>
2022-01-10 11:37:17 +0100

    Text renders now!

--- graphics_test/main/main.c
+++ graphics_test/main/main.c
@@ -32,11 +32,21 @@
 #define DISPLAY_CS 16
 #define DISPLAY_DCX 17
 
+#include "../components/pax-graphics-lib/test-images/empty.c"
+
 ILI9341 display;
 
-uint8_t *framebuffer;
+uint8_t framebuffer[ILI9341_BUFFER_SIZE];
 
 static const char *TAG = "main";
+
+pax_col_t test_shader_callback(pax_col_t tint, int x, int y, float u, float v, void *args) {
+	if (!args) {
+		return (u < 0.5) ^ (v >= 0.5) ? 0xffff00ff : 0xff1f1f1f;
+	}
+	pax_buf_t *image = (pax_buf_t *) args;
+	return pax_get_pixel(image, u*image->width, v*image->height);
+}
 
 void app_main() {
 	
@@ -77,49 +87,77 @@ void app_main() {
 	}
 	
 	// Initialise GFX.
-	framebuffer = (uint8_t *) malloc(ILI9341_BUFFER_SIZE);
+	ESP_LOGI(TAG, "Loading test image.");
+	pax_buf_t image;
+	pax_buf_init(&image, image_empty, 25, 25, PAX_BUF_32_8888ARGB);
 	
 	// Send a test pattern.
+	ESP_LOGI(TAG, "Creating framebuffer.");
 	pax_buf_t buf;
 	pax_buf_init(&buf, framebuffer, ILI9341_WIDTH, ILI9341_HEIGHT, PAX_BUF_16_565RGB);
+	if (pax_last_error) {
+		ESP_LOGE(TAG, "Framebuffer creation failed.");
+		return;
+	}
 	pax_apply_2d(&buf, matrix_2d_translate(buf.width / 2.0, buf.height / 2.0));
-	pax_apply_2d(&buf, matrix_2d_scale(50, 50));
+	// pax_apply_2d(&buf, matrix_2d_scale(2, 2));
+	
+	pax_shader_t test_shader = {
+		.callback = test_shader_callback,
+		.callback_args = &image
+	};
+	// pax_debug(&buf);
+	
 	while (1) {
 		uint64_t millis = esp_timer_get_time() / 1000;
 		
 		pax_background(&buf, 0xff000000);
-		pax_col_t color0 = 0xffffffff & pax_col_hsv(millis * 255 / 8000, 255, 255);
-		pax_col_t color1 = 0x7fffffff & pax_col_hsv(millis * 255 / 8000 + 127, 255, 255);
+		pax_col_t color0 =   0xffffffff & pax_col_hsv(millis * 255 / 8000, 255, 255);
+		pax_col_t color1 =   0x7fffffff & pax_col_hsv(millis * 255 / 8000 + 127, 255, 255);
 		
+		// Single triangle demo.
 		// uint32_t a = millis / 16;
-		// pax_simple_tri(&buf, color0, 0, 0, a % buf.width, buf.height / 2.0, buf.width / 2.0, a % buf.height);
-		// pax_simple_rect(&buf, color1, a % buf.width - 10, buf.height / 2.0 - 10, 20, 20);
-		// pax_simple_rect(&buf, color1, buf.width / 2.0 - 10, a % buf.height - 10, 20, 20);
+		//pax_clip(&buf, 0, 0, buf.width-((a >> 2) % buf.width), buf.height-((a >> 1) % buf.height));
+		//pax_simple_rect(&buf, 0xff000000, 0, 0, buf.width, buf.height);
+		// pax_draw_tri(&buf, -1, 0, 0, a % buf.width, buf.height / 2.0, buf.width / 2.0, a % buf.height);
 		
+		// Shading demo.
+		pax_push_2d(&buf);
 		float a0 = millis / 3000.0 * M_PI;
-		float a1 = fmodf(a0, M_PI * 4) - M_PI * 2;
-		pax_draw_arc(&buf, color0, 0, 0, 1, a0, a0 + a1);
-		pax_push_2d(&buf);
-		
 		pax_apply_2d(&buf, matrix_2d_rotate(a0));
-		pax_push_2d(&buf);
-		pax_apply_2d(&buf, matrix_2d_translate(1, 0));
-		pax_draw_rect(&buf, color1, -0.25, -0.25, 0.5, 0.5);
+		pax_shade_rect(&buf, -1, &test_shader, NULL, -25, -25, 50, 50);
 		pax_pop_2d(&buf);
+		char *text = "This is, a TEXT.";
+		pax_vec1_t size = pax_text_size(NULL, 18, text);
+		pax_draw_text(&buf, -1, NULL, 18, -size.x / 2, buf.height / 2 - size.y, "This is, a TEXT.");
 		
-		pax_apply_2d(&buf, matrix_2d_rotate(a1));
-		pax_push_2d(&buf);
-		pax_apply_2d(&buf, matrix_2d_translate(1, 0));
-		pax_apply_2d(&buf, matrix_2d_rotate(-a0 - a1 + M_PI * 0.5));
-		pax_draw_tri(&buf, color1, 0.25, 0, -0.125, 0.2165, -0.125, -0.2165);
-		pax_pop_2d(&buf);
+		// Epic arcs demo.
+		// float a0 = millis / 3000.0 * M_PI;
+		// float a1 = fmodf(a0, M_PI * 4) - M_PI * 2;
+		// pax_draw_arc(&buf, color0, 0, 0, 1, a0, a0 + a1);
+		// pax_push_2d(&buf);
 		
-		pax_pop_2d(&buf);
+		// pax_apply_2d(&buf, matrix_2d_rotate(a0));
+		// pax_push_2d(&buf);
+		// pax_apply_2d(&buf, matrix_2d_translate(1, 0));
+		// pax_draw_rect(&buf, color1, -0.25, -0.25, 0.5, 0.5);
+		// pax_pop_2d(&buf);
+		
+		// pax_apply_2d(&buf, matrix_2d_rotate(a1));
+		// pax_push_2d(&buf);
+		// pax_apply_2d(&buf, matrix_2d_translate(1, 0));
+		// pax_apply_2d(&buf, matrix_2d_rotate(-a0 - a1 + M_PI * 0.5));
+		// pax_draw_tri(&buf, color1, 0.25, 0, -0.125, 0.2165, -0.125, -0.2165);
+		// pax_pop_2d(&buf);
+		
+		// pax_pop_2d(&buf);
 		
 		if (ili9341_write(&display, framebuffer)) {
 			ESP_LOGE(TAG, "Display write failed.");
 			vTaskDelay(3000 / portTICK_PERIOD_MS);
 			esp_restart();
 		}
+		
+		// if (millis > 1500) break;
 	}
 }
