dc6ae9ecc5b7df3417fb55bc54ef60c7bfc25428
RobotMan2412 <julian@scheffers.net>
2022-01-20 15:56:42 +0100

    Some new features, some new docs tested.

--- graphics_test/main/main.c
+++ graphics_test/main/main.c
@@ -40,9 +40,59 @@
 
 ILI9341 display;
 
+// Pallette test, just 8bpp for nou.
+uint8_t pallette_test_img[] = {
+	0,1,0,1,0,
+	0,0,2,0,0,
+	1,0,0,0,1,
+	0,1,1,1,0
+};
+
+pax_col_t pallette_test_pal[] = {
+	pax_col_argb(0,   0,   0,   0),
+	pax_col_argb(255, 255, 255, 255),
+	pax_col_argb(255, 127, 127, 0)
+};
+
 uint8_t framebuffer[ILI9341_BUFFER_SIZE];
 
 static const char *TAG = "main";
+
+pax_col_t regenboogkots(pax_col_t tint, int x, int y, float u, float v, void *args) {
+	return pax_col_hsv(x / 50.0 * 255.0 + y / 150.0 * 255.0, 255, 255);
+}
+
+void my_graphics_function() {
+    // Setup.
+    pax_buf_t buffer;
+    pax_buf_init(&buffer, framebuffer, 320, 240, PAX_BUF_16_565RGB);
+    
+    // Green background.
+    pax_background(&buffer, pax_col_rgb(0, 255, 0));
+    
+    // Red circle.
+    float midpoint_x = buffer.width  / 2.0; // Middle of the screen horizontally.
+    float midpoint_y = buffer.height / 2.0; // Middle of the screen vertically.
+    float radius     = 50;                  // Nice, big circle.
+    pax_simple_circle(&buffer, pax_col_rgb(255, 0, 0), midpoint_x, midpoint_y, radius);
+    
+    // White text.
+    float text_x     = 0;                   // Top left corner of the screen.
+    float text_y     = 0;                   // Top left corner of the screen.
+    char *my_text    = "Hello, World!";     // You can pick any message you'd like.
+    float text_size  = 18;                  // Twice the normal size for "7x9".
+    pax_draw_text(&buffer, pax_col_rgb(255, 255, 255), PAX_FONT_DEFAULT, text_size, text_x, text_y, my_text);
+    
+    // Put it on the screen.
+    if (ili9341_write(&display, buffer.buf)) {
+        ESP_LOGE("my_tag", "Display write failed.");
+    } else {
+        ESP_LOGI("my_tag", "Display write success.");
+    }
+    
+    // Cleanup.
+    pax_buf_destroy(&buffer);
+}
 
 void app_main() {
 	
@@ -85,11 +135,17 @@ void app_main() {
 	// Initialise GFX.
 	ESP_LOGI(TAG, "Loading test image.");
 	pax_buf_t image;
-	pax_buf_init(&image, image_empty, 25, 25, PAX_BUF_32_8888ARGB);
-	ESP_LOGI(TAG, "Creating test buffer.");
-	pax_buf_t conv;
-	pax_buf_init(&conv, NULL, 25, 25, PAX_BUF_32_8888ARGB);
-	pax_buf_convert(&conv, &image, PAX_BUF_8_2222ARGB);
+	// pax_buf_init(&image, image_empty, 25, 25, PAX_BUF_32_8888ARGB);
+	pax_buf_init(&image, pallette_test_img, 5, 4, PAX_BUF_8_PAL);
+	image.pallette = pallette_test_pal;
+	image.pallette_size = sizeof(pallette_test_pal) / sizeof(pax_col_t);
+	// ESP_LOGI(TAG, "Creating test buffer.");
+	// pax_buf_t conv;
+	// pax_buf_init(&conv, NULL, 25, 25, PAX_BUF_32_8888ARGB);
+	// pax_buf_convert(&conv, &image, PAX_BUF_8_2222ARGB);
+	
+	// my_graphics_function();
+	// return;
 	
 	// Send a test pattern.
 	ESP_LOGI(TAG, "Creating framebuffer.");
@@ -100,7 +156,7 @@ void app_main() {
 		return;
 	}
 	
-	pax_debug(&conv);
+	// pax_debug(&conv);
 	
 	menu_entry_t menu_entries[] = {
 		{ .text = "OHM2013" },
@@ -114,18 +170,25 @@ void app_main() {
 		.font_size = 18
 	};
 	
+	pax_shader_t kots = {
+		.callback = regenboogkots
+	};
+	
 	pax_background(&buf, 0xff000000);
+	
+	pax_push_2d(&buf);
+	pax_pop_2d(&buf);
 	
 	while (1) {
 		uint64_t millis = esp_timer_get_time() / 1000;
 		
-		pax_col_t color0 =   0xffffffff & pax_col_hsv(millis * 255 / 8000 + 127, 255, 255);
-		pax_col_t color1 =   0xffffffff & pax_col_hsv(millis * 255 / 8000, 255, 255);
+		pax_col_t color0 = pax_col_hsv(millis * 255 / 8000 + 127, 255, 255);
+		pax_col_t color1 = pax_col_hsv(millis * 255 / 8000, 255, 255);
 		
 		// Single triangle demo.
 		// uint32_t a = millis / 16;
-		//pax_clip(&buf, 0, 0, buf.width-((a >> 2) % buf.width), buf.height-((a >> 1) % buf.height));
-		//pax_simple_rect(&buf, 0xff000000, 0, 0, buf.width, buf.height);
+		// pax_clip(&buf, 0, 0, buf.width-((a >> 2) % buf.width), buf.height-((a >> 1) % buf.height));
+		// pax_simple_rect(&buf, 0xff000000, 0, 0, buf.width, buf.height);
 		// pax_draw_tri(&buf, -1, 0, 0, a % buf.width, buf.height / 2.0, buf.width / 2.0, a % buf.height);
 		
 		// Shading demo.
@@ -147,8 +210,8 @@ void app_main() {
 		pax_draw_rect(&buf, 0xff000000, -1.3, -1.3, 2.6, 2.6);
 		
 		pax_apply_2d(&buf, matrix_2d_rotate(-a2));
-		// pax_shade_arc(&buf, color0, &PAX_SHADER_TEXTURE(&image), NULL, 0, 0, 1, a0 + a2, a0 + a1 + a2);
-		// pax_shade_circle(&buf, color0, &PAX_SHADER_TEXTURE(&image), NULL, 0, 0, 1);
+		// pax_shade_arc(&buf, color0, &kots, NULL, 0, 0, 1, a0 + a2, a0 + a1 + a2);
+		// pax_shade_circle(&buf, -1, &PAX_SHADER_TEXTURE(&image), NULL, 0, 0, 1);
 		pax_draw_arc(&buf, color0, 0, 0, 1, a0 + a2, a0 + a1 + a2);
 		
 		pax_apply_2d(&buf, matrix_2d_rotate(a0 + a2));
